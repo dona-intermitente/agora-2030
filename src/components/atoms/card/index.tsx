5a1fa71854cffb84cbf563a5cc05d4702ab02cc5
Sindi <sindidaconceicao@gmail.com>
2024-04-01 15:22:13 -0400

    style(programs): add card styles

--- src/components/atoms/card/index.tsx
+++ src/components/atoms/card/index.tsx
@@ -1,29 +1,39 @@
 import React, { FC } from 'react'
 import { Card as CardUi, CardBody, Image, CardFooter } from '@nextui-org/react'
 import Link from 'next/link'
+import { cn } from '@/lib/cn'
 
 type CardProps = {
   description: string
   href: string
+  hoverColor?: string
   img: string
   title: string
 }
 
-export const Card: FC<CardProps> = ({ description, href, img, title }) => {
+export const Card: FC<CardProps> = ({
+  description,
+  href,
+  hoverColor = '',
+  img,
+  title
+}) => {
   return (
     <Link href={href}>
-      <CardUi className='h-full'>
+      <CardUi
+        className={cn('h-full', hoverColor, {
+          'transition ease-in-out duration-300 hover:text-white': hoverColor
+        })}>
         <CardBody className='flex-none p-0'>
           <Image
+            alt={title}
+            className='object-cover'
             isZoomed
             radius='lg'
-            width='100%'
-            alt={title}
-            className='w-full object-cover'
             src={img}
           />
         </CardBody>
-        <CardFooter className='flex-col gap-4 h-full p-8'>
+        <CardFooter radioGroup='lg' className='flex-col h-full items-start p-8'>
           <h3>{title}</h3>
           <p>{description}</p>
         </CardFooter>
--- src/components/organisms/programs/index.tsx
+++ src/components/organisms/programs/index.tsx
@@ -4,7 +4,7 @@ import { programItems } from './utils'
 
 export const Programs: FC = () => {
   return (
-    <section className='gap-6 grid grid-cols-2 sm:grid-cols-3'>
+    <section className='gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
       {programItems.map((item, idx) => (
         <Card key={idx} {...item} />
       ))}
