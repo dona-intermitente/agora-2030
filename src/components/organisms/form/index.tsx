1333759c6222cb73e015afb6e5cb0b12f2e268f2
Sindi <sindidaconceicao@gmail.com>
2024-04-01 16:48:57 -0400

    style(form): add section form styles

--- src/components/organisms/form/index.tsx
+++ src/components/organisms/form/index.tsx
@@ -6,10 +6,10 @@ import React, { FC } from 'react'
 export const Form: FC = () => {
   return (
     <Card className='dark bg-accent'>
-      <CardBody className='container py-24'>
-        <form>
-          <h1>Subscribe to Our Newsletter</h1>
-          <div className='grid items-center grid-cols-2 gap-10'>
+      <CardBody className='content py-24'>
+        <h1>Subscribe to Our Newsletter</h1>
+        <form className='flex flex-col gap-10'>
+          <div className='grid md:grid-cols-2 md:gap-10 items-center'>
             <div>
               <Input type='text' label='First Name' />
               <Input type='text' label='Last Name' />
@@ -21,7 +21,9 @@ export const Form: FC = () => {
               <Input type='email' label='Country' />
             </div>
           </div>
-          <Button>Submit</Button>
+          <div>
+            <Button>Submit</Button>
+          </div>
         </form>
       </CardBody>
     </Card>
