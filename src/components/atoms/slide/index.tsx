2667c469ac1d77bb4da08af93a71c64acb45c127
Sindi <sindidaconceicao@gmail.com>
2024-04-05 06:52:51 -0400

    style(slide): add style responsive slide

--- src/components/atoms/slide/index.tsx
+++ src/components/atoms/slide/index.tsx
@@ -17,7 +17,27 @@ export const Slide: FC<SlideProps> = ({ items }) => {
     pauseOnHover: false,
     slidesToScroll: 1,
     slidesToShow: 5,
-    speed: 2000
+    speed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   }
 
   return (
